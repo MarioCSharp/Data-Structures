7f1ad02af8c0cca653a1b6f64238d463a801a189
Mario Petkov <mario.18@abv.bg>
2022-12-19 19:30:27 +0200

    2-3

---
+++ Tree Data Structures/2-3 Tree/TreeNode.cs
@@ -0,0 +1,35 @@
+﻿namespace _01.Two_Three
+{
+    using System;
+
+    public class TreeNode<T> 
+        where T : IComparable<T>
+    {
+        public T LeftKey;
+        public T RightKey;
+
+        public TreeNode<T> LeftChild;
+        public TreeNode<T> MiddleChild;
+        public TreeNode<T> RightChild;
+
+        public TreeNode(T key)
+        {
+            this.LeftKey = key;
+        }
+
+        public bool IsThreeNode()
+        {
+            return !RightKey.Equals(default(T));
+        }
+
+        public bool IsTwoNode()
+        {
+            return RightKey.Equals(default(T));
+        }
+
+        public bool IsLeaf()
+        {
+            return this.LeftChild == null && this.MiddleChild == null && this.RightChild == null;
+        }
+    }
+}
